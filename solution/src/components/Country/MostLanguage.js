bb3715d421288ba4c79f02246d2dc837fc695625
Barış KAYA <bkaya0199@gmail.com>
2021-07-24 14:29:50 +0300

    Update MostLanguage.js

--- solution/src/components/Country/MostLanguage.js
+++ solution/src/components/Country/MostLanguage.js
@@ -3,14 +3,13 @@ import MostLanguageCard from './MostLanguageCard';
 
  function MostLanguage(props) {
     const {countries} = props; 
-    const {limit} = 10;
     const [languages, setLanguages] = useState([]); 
 
     useEffect(() => {    
         let langs = []; 
         countries.map((country, index) => {  
             return country.languages;
-        }).map((_langs, i) => { 
+        }).forEach((_langs, i) => { 
 
             _langs.forEach(_lang => { 
                 let findIndex = langs.findIndex(lng => lng.iso639_1 === _lang.iso639_1);
@@ -23,7 +22,7 @@ import MostLanguageCard from './MostLanguageCard';
         });   
         langs.sort((a, b) => (a.count < b.count) ? 1 : -1);
         setLanguages(langs);   
-    }, []);
+    }, [countries]);
 
     return (
         <div className="row mt-5 mb-5">  
